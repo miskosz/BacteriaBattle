e67288cf3b2e3919b4b28d3c7f732201c4f2df68
Michal Szabados <misko.sz@gmail.com>
2014-10-21 23:41:04 +0300

    Simple menu and game scene

---
+++ Assets/Scripts/MenuBuilder.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuBuilder : MonoBehaviour {
+
+	void OnGUI() {
+
+		// temporary button size & placement
+		float buttonWidth = Screen.width / 2.0f;
+		float buttonHeight = 50;
+		Rect buttonRect = new Rect (
+			Screen.width / 2.0f - buttonWidth / 2.0f,
+			Screen.height / 2.0f - buttonHeight / 2.0f,
+			buttonWidth,
+			buttonHeight);
+
+		// button Start Game
+		if (GUI.Button(buttonRect, "Start Game")) {
+			Application.LoadLevel("GameScene");
+		}
+	}
+}
