ff6193c99778407848b2eb279c607641454522a7
Michal Szabados <misko.sz@gmail.com>
2014-10-26 16:26:00 +0200

    Board creation and clicking early prototype

---
+++ Assets/Scripts/BoardBuilderController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardBuilderController : MonoBehaviour {
+
+	// game board (TODO: state structure)
+	int boardSize = 4;
+	int[,] board;
+	Vector3 iVector = new Vector3(Mathf.Cos(-Mathf.PI/6),Mathf.Sin(-Mathf.PI/6),0);
+	Vector3 jVector = new Vector3(0,1,0);
+
+	public Transform boardCellPrefab;
+
+	void Start () {
+		// init the board
+		board = new int[2*boardSize-1, 2*boardSize-1];
+
+		for (int i = 0; i < 2*boardSize-1; i++) {
+			for (int j = 0; j < 2*boardSize-1; j++) {
+
+				// cut off corners
+				if (i-j >= boardSize || j-i >= boardSize)
+					board[i,j] = 0; // set empty
+				else
+					board[i,j] = 1; // set cell of type 1
+			}
+		}
+
+		// instantiate board cells
+		Debug.Log("here" + jVector);
+		
+		for (int i = 0; i < 2*boardSize-1; i++) {
+			for (int j = 0; j < 2*boardSize-1; j++) {
+				if (board[i,j] == 1) {
+					Vector3 position = (i-boardSize+1)*iVector + (j-boardSize+1)*jVector;
+					Instantiate(boardCellPrefab, position, Quaternion.identity);
+				}
+			}
+		}
+	}
+
+}
---
+++ Assets/Scripts/BoardCellController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardCellController : MonoBehaviour {
+
+    public Sprite blueBacteriaSprite;
+    public Sprite orangeBacteriaSprite;
+    bool blueShowing = true;
+
+    void OnMouseDown() {
+        // switch the sprite
+        blueShowing = !blueShowing;
+
+        if (blueShowing)
+			gameObject.GetComponent<SpriteRenderer>().sprite = blueBacteriaSprite;
+        else
+			gameObject.GetComponent<SpriteRenderer>().sprite = orangeBacteriaSprite;
+    }
+}
