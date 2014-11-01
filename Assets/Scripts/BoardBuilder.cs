a8d092711c3ae9795958e0e91ac12315868f1c83
Michal Szabados <misko.sz@gmail.com>
2014-11-01 18:35:38 +0200

    Board builder, board cell state

---
+++ Assets/Scripts/BoardBuilder.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardBuilder : MonoBehaviour {
+
+	public GameObject boardCellPrefab;
+	public float cellDistance = 1;
+
+	// initial board setup
+	// 0 - out of board
+	// 1 - empty
+	// 2 - blue
+	// 3 - orange
+	int[,] boardSetup = {
+		{1,1,1,2,0,0,0},
+		{1,1,1,1,1,0,0},
+		{1,1,1,1,1,1,0},
+		{1,1,1,1,1,1,1},
+		{0,1,1,1,1,1,1},
+		{0,0,1,1,1,1,1},
+		{0,0,0,3,1,1,1}
+	};
+
+	// game board
+	BoardCell[,] board;
+
+	void Start () {
+		//instance = this;
+
+		// compute layout vectors
+		Vector3 iVector = new Vector3 (0, -cellDistance, 0);
+		Vector3 jVector = new Vector3(Mathf.Cos(-Mathf.PI/6) * cellDistance, -Mathf.Sin(-Mathf.PI/6) * cellDistance, 0);
+		int iCenter = 3;
+		int jCenter = 3;
+
+		// init the board
+		board = new BoardCell[7,7];
+
+		for (int i = 0; i < board.GetLength(0); i++) {
+			for (int j = 0; j < board.GetLength(1); j++) {
+
+				// skip if positions is out of bounds
+				if (boardSetup[i,j] == 0)
+					continue;
+
+				// determine cell state
+				BoardCellState state;
+				switch (boardSetup[i,j]) {
+				case 1: state = BoardCellState.Empty; break;
+				case 2: state = BoardCellState.Blue; break;
+				case 3: state = BoardCellState.Orange; break;
+				default: throw new UnityException("Invalid board setup cell state.");
+				}
+
+				// create board cell
+				//Debug.Log("Initializing " + i + " " + j);
+				board[i,j] = ((GameObject) Instantiate(boardCellPrefab)).GetComponent<BoardCell>();
+				board[i,j].transform.position = (i-iCenter)*iVector + (j-jCenter)*jVector;
+				board[i,j].Initialize(state);
+			}
+		}
+	}
+
+}
---
+++ Assets/Scripts/BoardCell.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum BoardCellState {Blue, Orange, Empty};
+
+public class BoardCell : MonoBehaviour {
+
+    public Sprite blueBacteriaSprite;
+    public Sprite orangeBacteriaSprite;
+	BoardCellState state;
+
+	// called from BoardCellController when creating the board
+	public void Initialize(BoardCellState _state) {
+		state = _state;
+		UpdateImage ();
+	}
+
+    void OnMouseDown() {
+        // switch the sprite
+		state = (BoardCellState)(((int)state + 1) % 3);
+		UpdateImage ();
+    }
+
+	void UpdateImage() {
+		if (state == BoardCellState.Blue)
+			gameObject.GetComponent<SpriteRenderer>().sprite = blueBacteriaSprite;
+		else if (state == BoardCellState.Orange)
+			gameObject.GetComponent<SpriteRenderer>().sprite = orangeBacteriaSprite;
+		else
+			gameObject.GetComponent<SpriteRenderer>().sprite = null;
+	}
+}
