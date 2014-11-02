4cffb91cca1256329afb22bb82b4cca4b52fde44
Michal Szabados <misko.sz@gmail.com>
2014-11-02 21:21:15 +0200

    Game logic

--- Assets/Scripts/BoardBuilder.cs
+++ Assets/Scripts/BoardBuilder.cs
@@ -24,10 +24,15 @@ public class BoardBuilder : MonoBehaviour {
 	// game board
 	BoardCell[,] board;
 
+	// whose turn is it (0 or 1), switched after start
+	int playerOnTurn = 0;
+	BoardCellState[] playerState = {BoardCellState.Blue, BoardCellState.Orange};
+
 	void Start () {
 		//instance = this;
 
 		// compute layout vectors
+		// i - rows, j - columns
 		Vector3 iVector = new Vector3 (0, -cellDistance, 0);
 		Vector3 jVector = new Vector3(Mathf.Cos(-Mathf.PI/6) * cellDistance, -Mathf.Sin(-Mathf.PI/6) * cellDistance, 0);
 		int iCenter = 3;
@@ -56,9 +61,104 @@ public class BoardBuilder : MonoBehaviour {
 				//Debug.Log("Initializing " + i + " " + j);
 				board[i,j] = ((GameObject) Instantiate(boardCellPrefab)).GetComponent<BoardCell>();
 				board[i,j].transform.position = (i-iCenter)*iVector + (j-jCenter)*jVector;
-				board[i,j].Initialize(state);
+				board[i,j].Initialize(this, state, i, j);
 			}
+		}
+
+		nextTurn ();
+	}
+
+	void nextTurn() {
+		// switch player
+		playerOnTurn = 1 - playerOnTurn;
+
+		// detect closed areas
+		// TODO
+
+		// set highlighted places
+		// iterate over board cells
+		bool hasMove = false;
+
+		for (int i = 0; i < board.GetLength(0); i++) {
+			for (int j = 0; j < board.GetLength(1); j++) {
+				if (onBoard(i,j)) {
+					bool highlight = false;
+					if (board[i,j].getState() == BoardCellState.Empty) {
+						// iterate over cell neighbours
+						for (int d = 0; d < di.Length; d++) {
+							// get neighbour coordinates
+							int ii = i + di[d], jj = j + dj[d];
+							if (onBoard(ii, jj) && board[ii,jj].getState() == playerState[playerOnTurn]) {
+								highlight = true;
+								hasMove = true;
+								break;
+							}
+						}
+					}
+					board[i,j].setHighlighted(highlight);
+				}
+			}
+		}
+
+		// detect game end
+		if (!hasMove) {
+			Debug.Log("GAME OVER");
+
+			// all remaining cells are opponent's
+			for (int i = 0; i < board.GetLength(0); i++) {
+				for (int j = 0; j < board.GetLength(1); j++) {
+					if (onBoard(i,j) && board[i,j].getState() == BoardCellState.Empty) {
+						board[i,j].setState(playerState[1-playerOnTurn]);
+					}
+				}
+			}
+
+			// TODO
+
 		}
 	}
 
+	public void playerSelected(int i, int j) {
+
+		//Debug.Log ("Player selected " + i + " " + j);
+
+		// only moves to highlighted empty cells are valid
+		if (! board[i,j].getHighlighted() || board[i,j].getState() != BoardCellState.Empty)
+			return;
+
+		//Debug.Log ("It is a valid move.");
+
+		// new bacteria here, please!
+		board[i,j].setState(playerState[playerOnTurn]);
+
+		// convert neighbours
+		for (int d = 0; d < di.Length; d++) {
+			// get neighbour coordinates
+			int ii = i + di[d], jj = j + dj[d];
+			if (onBoard(ii,jj) && board[ii,jj].getState() == playerState[1-playerOnTurn]) {
+				board[ii,jj].setState(playerState[playerOnTurn]);
+			}
+		}
+
+		nextTurn ();
+	}
+
+	///
+	/// Board utilities
+	///
+
+	// neighbourhood vectors di, dj
+	// 1 2
+	// 0 . 3
+	//   5 4
+	int [] di = {0, -1, -1, 0, 1, 1};
+	int [] dj = {-1, -1, 0, 1, 1, 0};
+
+	bool onBoard(int i, int j) {
+		return (
+			0 <= i && i < board.GetLength(0) &&
+			0 <= j && j < board.GetLength(1) &&
+			boardSetup[i,j] != 0
+		);
+	}
 }
--- Assets/Scripts/BoardCell.cs
+++ Assets/Scripts/BoardCell.cs
@@ -1,24 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+//using System.Collections.Generic;
 
 public enum BoardCellState {Blue, Orange, Empty};
 
 public class BoardCell : MonoBehaviour {
 
     public Sprite blueBacteriaSprite;
-    public Sprite orangeBacteriaSprite;
+	public Sprite orangeBacteriaSprite;
+	public Sprite highlightedSprite;
+
+	// cell state
 	BoardCellState state;
 
+	// TODO: currently only empty cell can be highlighted,
+	// I do not know how to render two sprites independently.
+	bool highlighted = false;
+
+	// cell coordinates
+	int iPos, jPos;
+
+	// board builder
+	BoardBuilder boardBuilder;
+
 	// called from BoardCellController when creating the board
-	public void Initialize(BoardCellState _state) {
+	public void Initialize(BoardBuilder parent, BoardCellState _state, int _iPos, int _jPos) {
+		boardBuilder = parent;
 		state = _state;
+		iPos = _iPos;
+		jPos = _jPos;
 		UpdateImage ();
 	}
 
     void OnMouseDown() {
         // switch the sprite
-		state = (BoardCellState)(((int)state + 1) % 3);
-		UpdateImage ();
+		// state = (BoardCellState)(((int)state + 1) % 3);
+		// UpdateImage ();
+
+		boardBuilder.playerSelected (iPos, jPos);
     }
 
 	void UpdateImage() {
@@ -26,7 +45,24 @@ public class BoardCell : MonoBehaviour {
 			gameObject.GetComponent<SpriteRenderer>().sprite = blueBacteriaSprite;
 		else if (state == BoardCellState.Orange)
 			gameObject.GetComponent<SpriteRenderer>().sprite = orangeBacteriaSprite;
+		else if (highlighted)
+			gameObject.GetComponent<SpriteRenderer>().sprite = highlightedSprite;
 		else
 			gameObject.GetComponent<SpriteRenderer>().sprite = null;
 	}
+
+	// interface for highlighting
+	public bool getHighlighted() { return highlighted; }
+	public void setHighlighted(bool _highlighted = true) {
+		highlighted = _highlighted;
+		UpdateImage ();
+	}
+
+	// interface for state change
+	public BoardCellState getState() { return state; }
+	public void setState(BoardCellState _state) {
+		state = _state;
+		UpdateImage ();
+	}
+
 }
