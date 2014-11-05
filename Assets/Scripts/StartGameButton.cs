b548371cceaf5a4ecbfb02f90921dd0ec083b921
saminurmi <sami.nurmi@gmail.com>
2014-11-05 19:49:20 +0200

    Testing sounds in StartGameButton
    //There is two sounds at the moment, just for testing
    	//0=startup_sound
    	//1=fanfare

--- Assets/Scripts/StartGameButton.cs
+++ Assets/Scripts/StartGameButton.cs
@@ -3,7 +3,25 @@ using System.Collections;
 
 public class StartGameButton : MonoBehaviour {
 
+	//There is two sounds at the moment, just for testing
+	//0=startup_sound
+	//1=fanfare
+	//Not working
+	public AudioClip[] audioClip;
+
 	void OnMouseDown() {
+		Debug.Log("StartGameButton was pressed");
+		PlaySound (0);
 		Application.LoadLevel("GameScene");
+		Debug.Log("Audio play");
+
+	}
+
+
+	void PlaySound(int clip){
+		Debug.Log("Any sounds?");
+		audio.clip = audioClip [clip];
+		audio.Play();
+	
 	}
 }
