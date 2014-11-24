f74eb81954733c37a209430e18d51d81554e7967
Esa Aaltonen <estaalt@utu.fi>
2014-11-24 21:22:57 +0200

    Exit button changing, restart button placeholder

--- Assets/Scripts/ExitGameButton.cs
+++ Assets/Scripts/ExitGameButton.cs
@@ -3,7 +3,19 @@ using System.Collections;
 
 public class ExitGameButton : MonoBehaviour {
 
+	private SpriteRenderer spriteRenderer;
+	public Sprite exitDownSprite;
+
+	void Start () {
+
+		spriteRenderer = GetComponent<SpriteRenderer>();
+
+	}
+
 	void OnMouseDown() {
+
+		spriteRenderer.sprite = exitDownSprite;
+
 		Application.LoadLevel("MainMenu");
 	}
 }
