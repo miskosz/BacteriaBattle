75e37d4481fd7a70fb9ab9f1c8e45a89db6684e7
Michal Szabados <misko.sz@gmail.com>
2014-12-08 22:23:32 +0200

    Score counters

--- Assets/Scripts/GameGUI.cs
+++ Assets/Scripts/GameGUI.cs
@@ -4,22 +4,35 @@ using System.Collections;
 public class GameGUI : MonoBehaviour {
 
 	public BoardBuilder boardBuilder;
+	
+	public int labelBoxWidth = 40;
+	public int labelBoxHeight = 40;
 
-	public int orangex;
-	public int orangey;
+	public GUIStyle myStyle;
 
-	public int bluex;
-	public int bluey;
+	Transform player1SpriteTransform, player2SpriteTransform;
 
+	void Start() {
+		player1SpriteTransform = transform.FindChild("Player1ScoreSprite");
+		player2SpriteTransform = transform.FindChild("Player2ScoreSprite");
+
+		Vector3 tmp;
+		tmp = Camera.main.ScreenToWorldPoint(new Vector3 (0, Screen.height, 0));
+		player1SpriteTransform.position = new Vector3(tmp.x, tmp.y, 0);
+		player1SpriteTransform.localRotation = Quaternion.Euler(0, 0, 180f);
+
+		tmp = Camera.main.ScreenToWorldPoint(new Vector3 (Screen.width, 0, 0));
+		player2SpriteTransform.position = new Vector3(tmp.x, tmp.y, 0);
+	}
 
 	void OnGUI() {
 	
-		GUI.Label(new Rect(orangex, Screen.height-orangey, 100, 100),
-		          "<color=#f3c76f><size=50>"+boardBuilder.getScore(BoardCellState.Player2)+"</size></color>");
+		GUI.Label(new Rect(Screen.width - labelBoxWidth, Screen.height - labelBoxHeight, labelBoxWidth, labelBoxHeight),
+		          boardBuilder.getScore(BoardCellState.Player2).ToString(), myStyle);
 		
-		GUIUtility.RotateAroundPivot(-180, new Vector2(1, 1));
-		GUI.Label(new Rect(-Screen.width+bluex, bluey, 100, 100),
-		          "<color=#a6d2d1><size=50>"+boardBuilder.getScore(BoardCellState.Player1)+"</size></color>");
+		GUIUtility.RotateAroundPivot(-180, new Vector2(0,0));
+		GUI.Label(new Rect(-labelBoxWidth, -labelBoxHeight, labelBoxWidth, labelBoxHeight),
+		          boardBuilder.getScore(BoardCellState.Player1).ToString(), myStyle);
 
 
 
