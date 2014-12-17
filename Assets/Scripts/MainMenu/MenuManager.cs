a0daa4cd075bae9158713824d6059282c747feb0
saminurmi <saminurmi@macpro-2.local>
2014-12-17 23:41:50 +0200

    ToggleButton to Sounds
    I changed our design a little bit, I hope that it is ok. I might change
    the color of that mute button a little bit later.

---
+++ Assets/Scripts/MainMenu/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public bool musicOn = true;
+
+
+	public void ToggleMusic() {
+				musicOn = !musicOn;
+		Debug.Log ("Music boolean: "+musicOn);
+				if (musicOn == false) {
+						//MusicManagerSingleton.musicOff ();
+				} else {
+						//MusicManagerSingleton.musicOn ();
+				}
+		}
+
+
+}
+
+
