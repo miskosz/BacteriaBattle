dcf60867b15a960f25891da067975103ef95eef3
Michal Szabados <misko.sz@gmail.com>
2014-12-19 01:31:49 +0200

    Back button dynamic position

--- Assets/Scripts/MainMenu/BackButton.cs
+++ Assets/Scripts/MainMenu/BackButton.cs
@@ -2,6 +2,16 @@
 using System.Collections;
 
 public class BackButton : MonoBehaviour {
+
+	public float xOffset = 0.2f, yOffset = 0.4f;
+
+	void Start() {
+		transform.position = new Vector3 (
+			- Camera.main.orthographicSize / Screen.height * Screen.width + xOffset,
+			Camera.main.orthographicSize - yOffset,
+			0);
+	}
+
 	void OnMouseDown() {
 		Debug.Log("BackButton was pressed");
 		
