31449e2462192d3fca5a786f4045e8a3612fe36b
Michal Szabados <misko.sz@gmail.com>
2014-12-19 01:12:59 +0200

    Options - sound buttons using sprites

---
+++ Assets/Scripts/MainMenu/MusicToggle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicToggle : MonoBehaviour {
+
+	[SerializeField]
+	public Sprite[] offOnSprites = new Sprite[2];
+	
+	SpriteRenderer spriteRenderer;
+	
+	void Start () {
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		spriteRenderer.sprite = offOnSprites[MusicManagerSingleton.Instance.musicOn() ? 1 : 0];
+	}
+	
+	void OnMouseDown() {
+		MusicManagerSingleton.Instance.toggleMusic();
+		spriteRenderer.sprite = offOnSprites[MusicManagerSingleton.Instance.musicOn() ? 1 : 0];
+	}
+}
---
+++ Assets/Scripts/MainMenu/SoundsToggle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundsToggle : MonoBehaviour {
+
+	[SerializeField]
+	public Sprite[] offOnSprites = new Sprite[2];
+	
+	SpriteRenderer spriteRenderer;
+	
+	void Start () {
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		spriteRenderer.sprite = offOnSprites[MusicManagerSingleton.Instance.soundsOn() ? 1 : 0];
+	}
+	
+	void OnMouseDown() {
+		MusicManagerSingleton.Instance.toggleSound();
+		spriteRenderer.sprite = offOnSprites[MusicManagerSingleton.Instance.soundsOn() ? 1 : 0];
+	}
+}
